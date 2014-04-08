8f96c0b246decaf099bcef86de208ce32427cc18
tonywu522 <wukun522@gmail.com>
2014-04-08 01:42:00 -0400

    Modifed cookie check method. Adding the renew cookie expire time method.

--- WeedaForiPhone/WeedaForiPhone/WelcomeViewController.m
+++ WeedaForiPhone/WeedaForiPhone/WelcomeViewController.m
@@ -41,29 +41,25 @@
 - (void)viewDidAppear:(BOOL)animated
 {
     //check cookie and renew the cookie expire time
-    NSArray *cookies = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies];
-    if (!cookies || !cookies.count) {
+    NSHTTPCookie *cookie = [self checkCookies];
+    if (!cookie) {
         sleep(2);
         [self performSegueWithIdentifier:@"login" sender:self];
     } else {
+        //renew cookie expire time;
+        [self renewCookieExpireTime:cookie];
+        
         //check cookie authentication
         //success, go to MasterView
         self.currentUser = [NSEntityDescription
                             insertNewObjectForEntityForName:@"User"
                             inManagedObjectContext:[RKObjectManager sharedManager].managedObjectStore.mainQueueManagedObjectContext];
-        NSString *userId = nil;
-        for (NSHTTPCookie *cookie in cookies) {
-            if ([cookie.name isEqualToString:@"user_id"]) {
-                userId = cookie.value;
-            }
-            //renew cookie expire time;
-
-        }
-        self.currentUser.id = [NSNumber numberWithInt:[userId intValue]];
+        self.currentUser.id = [NSNumber numberWithInt:[cookie.value integerValue]];
+        
         sleep(2);
         [self performSegueWithIdentifier:@"masterView" sender:self];
         
-        //failure, go to LoginView
+        //TODO: failure, go to LoginView
         
     }
 }
@@ -83,9 +79,53 @@
     }
 }
 
+- (NSHTTPCookie *) checkCookies
+{
+    NSArray *cookies = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies];
+    if (!cookies || !cookies.count) {
+        return nil;
+    }
+    
+    if (cookies.count == 1) {
+        NSHTTPCookie *cookie = [cookies objectAtIndex:0];
+        if ([cookie.expiresDate earlierDate:[NSDate date]]) {
+            //cookie expired, delete cookie
+            [[NSHTTPCookieStorage sharedHTTPCookieStorage] deleteCookie:cookie];
+            return nil;
+        } else {
+            return cookie;
+        }
+    }
+    
+    //if there are more than one cookies, save the first one which not expired, delete others.
+    NSHTTPCookie *savedCookie = nil;
+    for (NSHTTPCookie *cookie in cookies) {
+        if (!savedCookie && [cookie.expiresDate laterDate:[NSDate date]]) {
+            savedCookie = cookie;
+        } else {
+            [[NSHTTPCookieStorage sharedHTTPCookieStorage] deleteCookie:cookie];
+        }
+    }
+    return savedCookie;
+}
+
 - (void) renewCookieExpireTime:(NSHTTPCookie *)cookie
 {
+    NSMutableDictionary *cookieProperties = [NSMutableDictionary dictionary];
+    [cookieProperties setObject:cookie.name forKey:NSHTTPCookieName];
+    [cookieProperties setObject:cookie.value forKey:NSHTTPCookieValue];
+    [cookieProperties setObject:cookie.path forKey:NSHTTPCookiePath];
+    [cookieProperties setObject:[NSString stringWithFormat:@"%d", cookie.version] forKey:NSHTTPCookieVersion];
     
+    // set expiration to one month from now or any NSDate of your choosing
+    // this makes the cookie sessionless and it will persist across web sessions and app launches
+    /// if you want the cookie to be destroyed when your app exits, don't set this
+    [cookieProperties setObject:[[NSDate date] dateByAddingTimeInterval:86400 * 7] forKey:NSHTTPCookieExpires];
+    
+    NSHTTPCookie *newCookie = [NSHTTPCookie cookieWithProperties:cookieProperties];
+    
+    [[NSHTTPCookieStorage sharedHTTPCookieStorage] deleteCookie:cookie];
+    [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:newCookie];
 }
 
 
